ba2544a6f6ebbd451ccd325caed243e603e49b15
Anuj Sharma <theanujsharma@outlook.com>
2022-01-21 15:59:12 +0530

    first commit

---
+++ Version_One/shop.py
@@ -0,0 +1,36 @@
+# menu = ["Pizza", "Burger", "Cookies"]
+menu = {"Pizza":100, "Burger":200, "Cookies":300}
+
+#printing menu
+for item, price in menu.items():
+    print(f'{item} ${price}')
+
+#decalaring variables
+order = "" 
+order_price = [] 
+bill = 0
+order_cart = []
+
+#taking orders 
+while order != "q":
+    order = input("Enter your order press q to quit: - ")
+    
+    #checking if order is in menu
+    if order in menu:
+        print(f"{order} costs ${menu[order]} has been added")
+        order_cart.append(order)
+        order_price.append(menu[order])
+
+    #when the user says q we are going to print the bill
+    elif order == "q":
+        print("Thanks for visiting")
+        print(f"You have  bought these items:-")
+        #prining the order cart
+        for item in order_cart:
+            print(item)
+        #calculating bills
+        for price in order_price:
+            bill = bill + price
+        print(f'Your order total is {bill}')
+    else:
+        print("Sorry, Its not available")
---
+++ Version_Three/food.py
@@ -0,0 +1,8 @@
+import display, order, database
+
+print('Welcome to the shop')
+menu = database.food
+#prinitng menu 
+display.print_list(menu)  # calling the function
+#taking order
+order.take_order(menu)
